7d454d25739d34e8e341663efb4a4246bfff3aad
Rajith Jayakuru <pandukajayakuru@gmail.com>
2021-10-28 17:22:34 -0400

    assert equal with temlate literals

--- assertEqual.js
+++ assertEqual.js
@@ -1,10 +1,10 @@
 // FUNCTION IMPLEMENTATION
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
-    console.log("Assertion passed:" + " " + actual + "" === "" + expected);
+    console.log(`Assertion passed: ${actual} === ${expected}`);
     
   } else  {
-    console.log("Assertion failed:" + " " + actual + "" !== "" + expected);
+    console.log(`Assertion passed: ${actual} !== ${expected}`)
   }
 
 };
