571b86af3f57b9fe08461115ec9e80a50d9456ef
Rajith Jayakuru <pandukajayakuru@gmail.com>
2021-10-29 01:09:57 -0400

    array comparison

--- eqArrays.js
+++ eqArrays.js
@@ -3,23 +3,23 @@ const assertEqual = function(actual, expected) {
     console.log(`Assertion passed: ${actual} === ${expected}`);
     
   } else  {
-    console.log(`Assertion failed: ${actual} !== ${expected}`)
+    console.log(`Assertion failed: ${actual} !== ${expected}`);
   }
 
 };
 
 const eqArrays = function(arr1, arr2) {
   if (arr1.length !== arr2.length) {
-    return false  
+    return false;
   }
 
   for (let i = 0; i < arr1.legth; i++) {
     if (arr1[i] !== arr2[i]) {
-      return false
+      return false;
     }
   }
-  return true
-} 
+  return true;
+};
 
 
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true)
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
