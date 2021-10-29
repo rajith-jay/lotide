4717dd0dc291aae9f5cef2d7467626bafd0bf71f
Rajith Jayakuru <pandukajayakuru@gmail.com>
2021-10-29 01:06:57 -0400

    array comparison

---
+++ eqArrays.js
@@ -0,0 +1,25 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`Assertion passed: ${actual} === ${expected}`);
+    
+  } else  {
+    console.log(`Assertion failed: ${actual} !== ${expected}`)
+  }
+
+};
+
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false  
+  }
+
+  for (let i = 0; i < arr1.legth; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false
+    }
+  }
+  return true
+} 
+
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true)
