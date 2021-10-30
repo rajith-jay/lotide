892e918d875b7037b9817d3ba26c324c577143f3
Rajith Jayakuru <pandukajayakuru@gmail.com>
2021-10-29 22:57:30 -0400

    takeUntil.js

---
+++ takeUntil.js
@@ -0,0 +1,16 @@
+//The function will return a "slice of the array with elements taken from the beginning."
+//It should keep going until the callback/predicate returns a truthy value.
+
+
+const takeUntil = function (array, callback) {
+  for (let i = 0; i < array.length; i++) {
+    if (callback(array[i])) {
+      return array.slice(0,i);
+    }
+  } 
+  return array;
+};
+
+const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
+const results1 = takeUntil(data1, x => x < 0);
+console.log(results1);
