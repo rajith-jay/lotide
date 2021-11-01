5830dd7a73d2a31de95150e6ac2d772347c1e2d2
Rajith Jayakuru <pandukajayakuru@gmail.com>
2021-11-01 15:22:39 -0400

    modularize code

--- assertEqual.js
+++ assertEqual.js
@@ -9,6 +9,6 @@ const assertEqual = function(actual, expected) {
 
 };
 
-// TEST CODE
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
+
+
+module.exports = assertEqual;
--- head.js
+++ head.js
@@ -1,12 +1,4 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`Assertion passed: ${actual} === ${expected}`);
-    
-  } else  {
-    console.log(`Assertion failed: ${actual} !== ${expected}`)
-  }
-
-};
+const assertEqual = require('./assertEqual');
 
 const head = function(arra) {
   
@@ -14,6 +6,3 @@ const head = function(arra) {
   }
 
 
-// TEST CODE
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
