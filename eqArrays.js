890fb02f1158be67013933bd7c9402a6c9253392
Rajith Jayakuru <pandukajayakuru@gmail.com>
2021-11-01 15:49:57 -0400

    modularized code

--- eqArrays.js
+++ eqArrays.js
@@ -1,19 +1,10 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`Assertion passed: ${actual} === ${expected}`);
-    
-  } else  {
-    console.log(`Assertion failed: ${actual} !== ${expected}`);
-  }
-
-};
+const assertEqual = require('./assertEqual');
 
 const eqArrays = function(arr1, arr2) {
   if (arr1.length !== arr2.length) {
     return false;
   }
-
-  for (let i = 0; i < arr1.legth; i++) {
+  for (let i = 0; i < arr1.length; i++) {
     if (arr1[i] !== arr2[i]) {
       return false;
     }
@@ -21,5 +12,4 @@ const eqArrays = function(arr1, arr2) {
   return true;
 };
 
-
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+module.exports = eqArrays;
