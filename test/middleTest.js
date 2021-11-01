25cf2acf736965654350c70f168b0b5e7fb94c42
Rajith Jayakuru <pandukajayakuru@gmail.com>
2021-11-01 17:03:41 -0400

    test files

---
+++ test/middleTest.js
@@ -0,0 +1,6 @@
+
+
+const middle = require('../middle');
+
+
+console.log(middle([1, 2, 3], [1, 2, 3]));
---
+++ test/tailTest.js
@@ -0,0 +1,7 @@
+const assertEqual = require('../assertEqual');
+
+const tail = require('../tail');
+
+const words = ["Yo Yo"];
+
+assertEqual(words.length, 3); // original array should still have 3 elements!
