2525987677aa27285d397d11457297d1c01f3a24
Rajith Jayakuru <pandukajayakuru@gmail.com>
2021-11-02 02:05:02 -0400

    Refactor with Mocha and Chai I

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,6 +1,15 @@
 
 
-const middle = require('../middle');
 
 
-console.log(middle([1, 2, 3], [1, 2, 3]));
+const assert = require('chai').assert;
+const middle   = require('../middle');
+
+describe("#middle", () => {
+  it('returns [2] for [1, 2, 3]', () => {
+    assert.deepEqual(middle([1, 2, 3]), [2]);
+  });
+  it("returns '5' for ['5']", () => {
+    assert.strictEqual(middle(['5']), '5'); 
+  });
+})
--- test/tailTest.js
+++ test/tailTest.js
@@ -1,7 +1,12 @@
-const assertEqual = require('../assertEqual');
 
-const tail = require('../tail');
+const assert = require('chai').assert;
+const tail   = require('../tail');
 
-const words = ["Yo Yo"];
-
-assertEqual(words.length, 3); // original array should still have 3 elements!
+describe("#tail", () => {
+  it('returns [2, 3] for [1, 2, 3]', () => {
+    assert.deepEqual(tail([1, 2, 3]), [2, 3]);
+  });
+  it("returns '5' for ['5']", () => {
+    assert.strictEqual(tail(['5']), '5'); 
+  });
+})
